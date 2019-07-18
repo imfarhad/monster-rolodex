9204e55fa2cc1dd58ac434b316b1e8394b2212a9
imfarhad <fakhrul.farhad@gmail.com>
2019-07-18 16:06:59 +0300

    Updates

---
+++ precache-manifest.9e45dc0c3b91ce8cf8e915703b69a3d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca87b780424076b26b2b115c86502bd7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "986fa940b4ffb69b4401",
+    "url": "/monster-rolodex/static/css/main.09e5ae88.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "986fa940b4ffb69b4401",
+    "url": "/monster-rolodex/static/js/main.36c63d3b.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
